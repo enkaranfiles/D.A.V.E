db2d132bff8d9c64f694187dba795db73469d0f2
eneskaranfil <enkaranfiles@gmail.com>
2020-11-24 17:04:53 +0300

    data crawl code review!

--- get-wiki-site.py
+++ get-wiki-site.py
@@ -1,22 +1,32 @@
 from bs4 import BeautifulSoup
 from bs4.element import Comment
 import urllib.request
+import argparse
 
-
-def tag_visible(element):
+def findParagraphTags(element):
     if element.parent.name in ['style', 'script', 'head', 'title', 'meta', '[document]']:
         return False
     if isinstance(element, Comment):
         return False
     return True
 
-
 def text_from_html(body):
     soup = BeautifulSoup(body, 'html.parser')
     texts = soup.findAll(text=True)
-    visible_texts = filter(tag_visible, texts)  
+    visible_texts = filter(findParagraphTags, texts)
     return u" ".join(t.strip() for t in visible_texts)
 
-html = urllib.request.urlopen("https://en.wikipedia.org/wiki/COVID-19_pandemic").read()
-file = open("Data/pandemic","w")
-file.write(text_from_html(html))
+
+def main(args):
+    html = urllib.request.urlopen(args.wiki_side).read()
+    output_path = args.output_path
+    file = open(output_path, "w")
+    file.write(text_from_html(html))
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--wiki_side',
+                        help='Webpage name to crawl', required=True)
+    parser.add_argument('--output_path',
+                        help='Path to the output file that D.A.V.E can learn something :) ', required=True)
+    main()
